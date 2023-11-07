041830df022c4ef7d35806f9d89200ccbdfed78d
saganorth <saga.nord99@hotmail.se>
2023-11-07 10:37:11 +0100

    first commit

---
+++ products.js
@@ -0,0 +1,18 @@
+
+
+const products = [
+  { id: 1, name: 'Christmas Tree', price: 100, stock: 10, description: 'Lovely tree' , imageUrl: '/img/tree.png'}, 
+  { id: 2, name: 'Christmas Sledge', price: 150, stock: 10, description: 'Red big sledge', imageUrl: '/img/sleigh.png' },
+  { id: 3, name: 'Christmas Lights', price: 50, stock: 10, description: 'to light up your house', imageUrl: '/img/lights.jpeg' }, 
+  { id: 4, name: 'Christmas Ornaments', price: 30, stock: 10, description: 'to hang in the tree' , imageUrl: '/img/ornaments.jpeg'},
+  { id: 5, name: 'Nutcracker', price: 60, stock: 10, description: 'to crush those nuts', imageUrl: '/img/nutcracker.jpeg' }, 
+  { id: 6, name: 'Christmas Star', price: 40, stock: 10, description: 'Shining bright', imageUrl: '/img/star.jpeg' },
+  { id: 7, name: 'Candlestick', price: 70, stock: 10, description: 'for advent', imageUrl: '/img/candles.jpeg' }, 
+  { id: 8, name: 'Gingerbread', price: 10, stock: 10, description: 'to eat', imageUrl: '/img/gingerbread.jpeg' }
+  
+];
+
+
+
+
+module.exports = products;
